b55296cfbeb8df9b636069b18252a382410f184c
Andrew Beveridge <andrew@beveridge.uk>
2019-02-11 02:03:42 +0000

    Added test for finding SQL files in directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,39 +3,39 @@ import pytest
 
 @pytest.fixture
 def sql_filename_expected():
-    return "010.test_name.sql"
+    return "045.createtable.sql"
 
 
 @pytest.fixture
 def sql_filename_hyphen():
-    return "010-test_name.sql"
+    return "045-createtable.sql"
 
 
 @pytest.fixture
 def sql_filename_not_zero_padded():
-    return "10.test_name.sql"
+    return "45.createtable.sql"
 
 
 @pytest.fixture
 def sql_filename_spaced():
-    return "10 .test_name.sql"
+    return "45 .createtable.sql"
 
 
 @pytest.fixture
 def sql_filename_no_sql_suffix():
-    return "10.test_name.jpg"
+    return "45.createtable.jpg"
 
 
 @pytest.fixture
 def sql_filename_no_separator():
-    return "10test_name.sql"
+    return "45createtable.sql"
 
 
 @pytest.fixture
 def sql_filename_bigint():
-    return "23514352834592347502351435283459234750.test_name.sql"
+    return "23514352834592347502351435283459234750.createtable.sql"
 
 
 @pytest.fixture
 def sql_filename_no_version():
-    return "test_name.sql"
+    return "createtable.sql"
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -5,35 +5,35 @@ import run_migrations as r
 
 def test_extract_sequence_sql_filename_expected(sql_filename_expected):
     version = r.extract_sequence_num(sql_filename_expected)
-    assert version == 10
+    assert version == 45
 
 
 def test_extract_sequence_sql_filename_hyphen(sql_filename_hyphen):
     version = r.extract_sequence_num(sql_filename_hyphen)
-    assert version == 10
+    assert version == 45
 
 
 def test_extract_sequence_sql_filename_not_zero_padded(
         sql_filename_not_zero_padded):
     version = r.extract_sequence_num(sql_filename_not_zero_padded)
-    assert version == 10
+    assert version == 45
 
 
 def test_extract_sequence_sql_filename_spaced(sql_filename_spaced):
     version = r.extract_sequence_num(sql_filename_spaced)
-    assert version == 10
+    assert version == 45
 
 
 def test_extract_sequence_sql_filename_no_sql_suffix(
         sql_filename_no_sql_suffix):
     version = r.extract_sequence_num(sql_filename_no_sql_suffix)
-    assert version == 10
+    assert version == 45
 
 
 def test_extract_sequence_sql_filename_no_separator(
         sql_filename_no_separator):
     version = r.extract_sequence_num(sql_filename_no_separator)
-    assert version == 10
+    assert version == 45
 
 
 def test_extract_sequence_sql_filename_bigint(
@@ -48,7 +48,22 @@ def test_extract_sequence_sql_filename_no_version(
         r.extract_sequence_num(sql_filename_no_version)
 
 
-def test_append_migration_to_list_sql_filename_expected(sql_filename_expected):
+def test_append_migration_sql_filename_expected(sql_filename_expected):
     migrations = []
-    r.append_migration_to_list(migrations, sql_filename_expected)
-    assert migrations == [(10, sql_filename_expected)]
+    r.append_migration(migrations, sql_filename_expected)
+    assert migrations == [(45, sql_filename_expected)]
+
+
+def test_append_migration_sql_filename_expected_existing_value(
+        sql_filename_expected):
+    migrations = [(2, "test.sql")]
+    r.append_migration(migrations, sql_filename_expected)
+    assert migrations == [(2, "test.sql"),
+                          (45, sql_filename_expected)]
+
+
+def test_find_migrations_in_directory_expected(tmpdir, sql_filename_expected):
+    filepath = tmpdir.join(sql_filename_expected)
+    filepath.write("test")
+    migrations = r.find_migrations_in_directory(str(tmpdir))
+    assert migrations == [(45, str(filepath))]
