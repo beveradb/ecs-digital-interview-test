25693e77758b9419ac494b8e5b334fa9ae584832
Andrew Beveridge <andrew@beveridge.uk>
2019-02-12 14:36:36 +0000

    Modified setuptools config to show readme on pypi

--- HISTORY.md
+++ HISTORY.md
@@ -1,7 +1,12 @@
 History
 =======
 
-0.3.0 (2019-02-12)
+0.3.1 (2019-02-11)
+------------------
+
+* Significantly refactored and improved docs
+
+0.3.0 (2019-02-11)
 ------------------
 
 * First release on PyPI.
--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@
 
 from setuptools import setup, find_packages
 
-# with open('README.md') as readme_file:
-#    readme = readme_file.read()
+with open('README.md') as readme_file:
+    readme = readme_file.read()
 
-# with open('HISTORY.md') as history_file:
-#    history = history_file.read()
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
 
 requirements = [
     "Click>=7.0",
@@ -19,20 +19,19 @@ requirements = [
 
 setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest', 'tox', 'flake8', 'coverage']
+test_requirements = ['pytest', 'tox', 'flake8', 'coverage', 'coveralls']
 
 setup(
     author="Andrew Beveridge",
     author_email='andrew@beveridge.uk',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -46,9 +45,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=
-    "Run MySQL migration scripts sequentially from a specified "
-    "directory, keeping track of current version in the database.",
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='migration_runner',
     name='migration_runner',
